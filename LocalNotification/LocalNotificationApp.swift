6f7d321b7319b715bdb2fd4aa2ac2f1aaad9b7a6
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-02-01 10:46:48 +0530

    Initial Commit

---
+++ LocalNotification/LocalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationApp.swift
+//  LocalNotification
+//
+//  Created by HEMANTH on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
