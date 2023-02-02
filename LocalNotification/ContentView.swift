39ead43d6fd10aeb2897a38420e61aa3242075cd
Hemanth Kumar <70285394+Hemanth-Kumar-G@users.noreply.github.com>
2023-02-02 18:42:25 +0530

    notification implemented

--- LocalNotification/ContentView.swift
+++ LocalNotification/ContentView.swift
@@ -6,14 +6,45 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+            Button("Request permission"){
+                UNUserNotificationCenter.current().requestAuthorization(
+                    options:  [.alert,.badge,.sound]) { success , error in
+                        if success {
+                            print("ALL set !!")
+                        } else if let error = error {
+                            print(error.localizedDescription)
+                        }
+                    }
+            }.padding()
+                .background(.pink.opacity(0.7))
+                .foregroundColor(.white)
+            
+            Spacer().frame(height: 40)
+            
+            Button("schedule Notification"){
+                let content = UNMutableNotificationContent()
+                
+                content.title = "Notification title here "
+                content.subtitle = "Notification sub title here "
+                content.sound = UNNotificationSound.default
+                content.launchImageName = "google"
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+                
+                let request = UNNotificationRequest(
+                    identifier:
+                        UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+                
+            }.padding()
+                .background(.red)
+                .foregroundColor(.white)
         }
         .padding()
     }
